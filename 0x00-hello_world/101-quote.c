25388a0d0f8bffd09d8960136b0bfea85468ed81
sadboypro <desireprosper1315@gmail.com>
2022-09-08 15:19:11 -0700

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,13 +5,8 @@
  *
  * Return: Always 0 (success)
  **/
-*main - Output to standard error
-*
-*Return: Always 0 (Success)
-**/
 int main(void)
 {
 	write(1, "and that piece of art is usefull\" - Dora Korpar, 2015-10-19\n", 59);
-	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
