c3f5edd18c05fc2922fceb29c80e75a800def036
sadboypro <desireprosper1315@gmail.com>
2022-09-08 12:28:36 -0700

    +

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - print the string in the function 
+ *
+ * Description: using the main function
+ * this program prints "Progamming is like building a multilingual puzzle
+ * Return: 0
+ */
+int main(void)
+{
+	puts("\"Progamming is like building a multilingual puzzle");
+	return (0);
+}	
