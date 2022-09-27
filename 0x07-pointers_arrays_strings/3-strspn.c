a864715c64c1feee439eea43941e546c2c2d9af5
sadboypro <desireprosper1315@gmail.com>
2022-09-27 04:08:21 -0700

    Task 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,9 +1,31 @@
 #include "main.h"
 
 /**
- * unsigned int _strspn(char *s, char *accept);
- * @strspn:
- *
- * Returns: 0
+ * _strspn - get length of a prefix substring
+ * @s: string
+ * @accept: bytes being compared
+ * Description: finds number of bytes required to find accept in s
+ * Return: bytes required to find accept
  */
+
 unsigned int _strspn(char *s, char *accept)
+{
+	int i, j, ex;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		ex = 0;
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (accept[j] == s[i])
+			{
+				ex = 1;
+			}
+		}
+		if (ex == 0)
+		{
+			return (i);
+		}
+	}
+	return (i);
+}
