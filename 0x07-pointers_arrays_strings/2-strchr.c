2d87b1dd2b8532910c5581b107b6e08c79b2f299
sadboypro <desireprosper1315@gmail.com>
2022-09-27 04:05:43 -0700

    Task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,10 +1,21 @@
 #include "main.h"
 
-
 /**
- * char *_strchr(char *s, char c);
- * @strchr:
- *
- * Returns: 0
+ * _strchr - locates a char in a string
+ * @s: string to be searched
+ * @c: character to be searched in str
+ * Return: number of characters
  */
+
 char *_strchr(char *s, char c)
+{
+	int index;
+
+	for (index = 0; s[index] >= '\0'; index++)
+	{
+		if (s[index] == c)
+			return (s + index);
+	}
+
+	return ('\0');
+}
